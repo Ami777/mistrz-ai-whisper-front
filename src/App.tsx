961a04d6fa1a08ea833743a8b12f648c117afc9e
JakubKrol <contact@itdotfocus.com>
2024-02-08 12:13:02 +0100

    Finished Whisper front-only.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useWhisper} from "./vendor/use-whisper-main/src";
+import {API_KEY} from "./config.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {startRecording, stopRecording, recording, transcript} = useWhisper({
+        apiKey: API_KEY,
+        autoTranscribe: true,
+        whisperConfig: {
+            language: 'pl',
+        },
+        removeSilence: true,
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>{recording ? '🔴' : '⏸️'}</h1>
+            <p>{transcript?.text ?? ''}</p>
+            <button onClick={startRecording}>Start</button>
+            <button onClick={stopRecording}>Stop</button>
+        </>
+    )
 }
 
 export default App
